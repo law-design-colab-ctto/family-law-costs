f8fb782bd46524c13d18c542ac4146247669b463
re-mint <babak.eric@gmail.com>
2018-09-17 04:02:43 -0400

    finalized

--- src/components/costs-income-total/costs-income-total.component.js
+++ src/components/costs-income-total/costs-income-total.component.js
@@ -13,38 +13,6 @@ export const CostsIncomeWithBars = ({ totalDirectCosts, income }) => (
       <LargeCostDisplay>{totalDirectCosts}</LargeCostDisplay>
     </CenteredContent>
 
-    {parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10)}
-    <br />
-    {parseInt(income.replace(/[,$]/g, ""), 10)}
-    <br />
-    {parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10) <
-     parseInt(income.replace(/[,$]/g, ""), 10) ?
-     "this: income should beb 100" :
-     "costs should be 100%"}
-    <br />
-    {parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10) <
-     parseInt(income.replace(/[,$]/g, ""), 10) ?
-     "this: income should be 100%" :
-     "costs should be 100%"}
-    <br />
-    For top bar of costs:
-    {
-      parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10) <
-      parseInt(income.replace(/[,$]/g, ""), 10) ?
-      parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10) /
-      parseInt(income.replace(/[,$]/g, ""), 10) * 100 :
-      100
-    }
-    <br />
-    For bottom bar of income:
-    {
-      parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10) <
-      parseInt(income.replace(/[,$]/g, ""), 10) ?
-      100 :
-      parseInt(income.replace(/[,$]/g, ""), 10) /
-      parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10) * 100
-    }
-
     Costs <BlueBar variant="determinate"
             value={
               parseInt(totalDirectCosts.replace(/[,$]/g, ""), 10) <
