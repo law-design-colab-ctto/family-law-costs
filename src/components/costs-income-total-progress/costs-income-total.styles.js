ebdf4454eff355dbf97f778e85a22bf6cbc0d6a4
re-mint <babak.eric@gmail.com>
2018-10-13 14:03:07 -0400

    duped component

---
+++ src/components/costs-income-total-progress/costs-income-total.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+export const CostsBar = styled(LinearProgress)`
+`
+
+export const IncomeBar = styled(LinearProgress)`
+`
